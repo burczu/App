65c089dc15757fe79be0681b3238d221f4a9c663
burczu <b.dybowski@gmail.com>
2023-06-07 08:54:26 +0200

    initial changes

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -210,7 +210,7 @@ export default compose(
             key: ONYXKEYS.COLLECTION.REPORT,
         },
         personalDetails: {
-            key: ONYXKEYS.PERSONAL_DETAILS,
+            key: ONYXKEYS.PERSONAL_DETAILS_LIST,
         },
         betas: {
             key: ONYXKEYS.BETAS,
