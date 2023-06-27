b734abe5f72bdd7c16d40150ca28652be9a21899
burczu <b.dybowski@gmail.com>
2023-06-27 07:43:24 +0200

    date utils tests

--- tests/unit/DateUtilsTest.js
+++ tests/unit/DateUtilsTest.js
@@ -1,11 +1,20 @@
-import moment from 'moment';
+import moment from 'moment-timezone';
 import Onyx from 'react-native-onyx';
 import CONST from '../../src/CONST';
-import DateUtils from '../../src/libs/DateUtils';
 import ONYXKEYS from '../../src/ONYXKEYS';
 import waitForPromisesToResolve from '../utils/waitForPromisesToResolve';
 
 const LOCALE = CONST.LOCALES.EN;
+const DEFAULT_TIME_ZONE = 'Etc/UTC';
+
+jest.mock('moment-timezone', () => {
+    const localMoment = jest.requireActual('moment-timezone');
+    localMoment.tz.guess = () => 'Etc/UTC';
+    return jest.fn(() => localMoment);
+});
+
+// eslint-disable-next-line import/first
+import DateUtils from '../../src/libs/DateUtils';
 
 describe('DateUtils', () => {
     beforeAll(() => {
@@ -13,17 +22,33 @@ describe('DateUtils', () => {
             keys: ONYXKEYS,
             initialKeyStates: {
                 [ONYXKEYS.SESSION]: {accountID: 999},
-                [ONYXKEYS.PERSONAL_DETAILS_LIST]: {999: {timezone: {selected: 'Etc/UTC'}}},
+                [ONYXKEYS.PERSONAL_DETAILS_LIST]: {999: {timezone: {selected: DEFAULT_TIME_ZONE}}},
             },
         });
         return waitForPromisesToResolve();
     });
 
+    beforeEach(() => {
+        jest.useFakeTimers().setSystemTime(new Date('2021-01-01'));
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
     const datetime = '2022-11-07 00:00:00';
+    // TODO: this will be rewritten when the moment library is removed
     it('should return a moment object with the formatted datetime when calling getLocalMomentFromDatetime', () => {
         const localMoment = DateUtils.getLocalMomentFromDatetime(LOCALE, datetime, 'America/Los_Angeles');
         expect(moment.isMoment(localMoment)).toBe(true);
         expect(moment(localMoment).format()).toEqual('2022-11-06T16:00:00-08:00');
+    });
+
+    it('should return moment with current timezone when date time not provided', () => {
+        const currentSelectedTimezone = 'America/Los_Angeles'
+        const localMoment = DateUtils.getLocalMomentFromDatetime(LOCALE, null, currentSelectedTimezone);
+        expect(moment.isMoment(localMoment)).toBe(true);
+        expect(moment(localMoment).format()).toEqual(moment.tz(currentSelectedTimezone).format());
     });
 
     it('should return the date in calendar time when calling datetimeToCalendarTime', () => {
@@ -37,6 +62,17 @@ describe('DateUtils', () => {
         expect(DateUtils.datetimeToCalendarTime(LOCALE, date)).toBe('Nov 5, 2022 at 10:17 AM');
     });
 
+    it('should return the date in calendar time, lower-cased, when calling datetimeToCalendarTime with lowercase param set', () => {
+        const today = moment.utc().set({hour: 14, minute: 32});
+        expect(DateUtils.datetimeToCalendarTime(LOCALE, today.format(), false, undefined, true)).toBe('today at 2:32 PM');
+
+        const yesterday = moment.utc().subtract(1, 'days').set({hour: 7, minute: 43});
+        expect(DateUtils.datetimeToCalendarTime(LOCALE, yesterday.format(), false, undefined, true)).toBe('yesterday at 7:43 AM');
+
+        const date = moment.utc('2022-11-05').set({hour: 10, minute: 17});
+        expect(DateUtils.datetimeToCalendarTime(LOCALE, date.format(), false, undefined, true)).toBe('Nov 5, 2022 at 10:17 AM');
+    });
+
     it('should return the date in calendar time when calling datetimeToRelative', () => {
         const aFewSecondsAgo = moment().subtract(10, 'seconds');
         expect(DateUtils.datetimeToRelative(LOCALE, aFewSecondsAgo)).toBe('a few seconds ago');
@@ -46,6 +82,39 @@ describe('DateUtils', () => {
 
         const anHourAgo = moment().subtract(1, 'hour');
         expect(DateUtils.datetimeToRelative(LOCALE, anHourAgo)).toBe('an hour ago');
+    });
+
+    it('should call addEventListener for all tracked events', () => {
+        Object.defineProperty(global.document, 'addEventListener', { value: jest.fn() });
+
+        DateUtils.startCurrentDateUpdater();
+        expect(global.document.addEventListener).toHaveBeenCalledTimes(4);
+    });
+
+    describe('getCurrentTimezone', () => {
+        it('should correct timezone', () => {
+            expect(DateUtils.getCurrentTimezone()).toEqual({selected: DEFAULT_TIME_ZONE});
+        });
+
+        it('should return correct timezone if automatic and different than selected', () => {
+            const testTimezone = 'America/Los_Angeles';
+            moment.tz.guess = jest.fn(() => testTimezone);
+            Onyx.init({
+                keys: ONYXKEYS,
+                initialKeyStates: {
+                    [ONYXKEYS.SESSION]: {accountID: 999},
+                    [ONYXKEYS.PERSONAL_DETAILS_LIST]: {999: {timezone: {selected: DEFAULT_TIME_ZONE, automatic: true}}},
+                },
+            });
+            return waitForPromisesToResolve().then(() => {
+                expect(DateUtils.getCurrentTimezone()).toEqual({selected: testTimezone, automatic: true});
+            });
+        });
+    });
+
+    it('should call moment when setTimezoneUpdated is called', () => {
+        DateUtils.setTimezoneUpdated();
+        expect(moment).toHaveBeenCalledTimes(1);
     });
 
     describe('getDBTime', () => {
